481d017e7024b12978d638594c2d1a4426081d6e
Prahlad Misra <mprahlad95@gmail.com>
2018-07-06 07:41:59 +0530

    added

---
+++ src/Medium/MergeTwoBT.java
@@ -0,0 +1,14 @@
+package Medium;
+
+public class MergeTwoBT {
+	public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+		if (t1 == null)
+			return t2;
+		if (t2 != null) {
+			t1.val += t2.val;
+			t1.left = mergeTrees(t1.left, t2.left);
+			t1.right = mergeTrees(t1.right, t2.right);
+		}
+		return t1;
+	}
+}
